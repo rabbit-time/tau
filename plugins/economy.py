d120f557f8b637dc8bd39901261e41bb292db753
Emily Hall <30204467+emisdumb@users.noreply.github.com>
2020-04-22 09:22:16 -0500

    minor changes

--- plugins/economy.py
+++ plugins/economy.py
@@ -13,7 +13,7 @@ class Economy(commands.Cog):
         self.bot = bot
 
     @commands.command(cls=perms.Lock, name='balance', aliases=['acc', 'bal'], usage='balance [member]')
-    async def balance(self, ctx, member: discord.Member = None):
+    async def balance(self, ctx, *, member: discord.Member = None):
         '''Display user account balance.
         This only works with members within the guild.\n
         **Example:```yml\n.balance\n.bal @Tau#4272\n.acc 608367259123187741```**
@@ -32,8 +32,7 @@ class Economy(commands.Cog):
 
     @commands.command(cls=perms.Lock, guild_only=True, name='give', usage='give <member> <quantity>')
     async def give(self, ctx, member: discord.Member, qty):
-        '''Give tickets to another user.
-        *mention* can also be a user ID.
+        '''Give credits to another user.
         *quantity* can also be '\\*' or 'all'\n
         **Example:```yml\n.give @Tau#4272 420 \n.give 608367259123187741 69```**
         '''
@@ -53,17 +52,17 @@ class Economy(commands.Cog):
         await ctx.send(f'**{ctx.author.display_name}** gave {emoji["tickets"]}**{qty}** to **{member.display_name}**!')
 
     @commands.cooldown(1, 86400.0, type=commands.BucketType.user)
-    @commands.command(cls=perms.Lock, name='tickets', aliases=['credits', 'daily', 'tic'], usage='tickets')
-    async def tickets(self, ctx):
-        '''Collect your daily tickets.
+    @commands.command(cls=perms.Lock, name='credits', aliases=['daily'], usage='tickets')
+    async def credits(self, ctx):
+        '''Collect your daily credits.
         Has a cooldown of 24 hours.\n
-        **Example:```yml\n.tickets```**
+        **Example:```yml\n.credits```**
         '''
-        msgs = ['Here are your tickets!', 'Gosh, you\'re just using me for money, aren\'t you?', ':money_with_wings: :money_with_wings:',
-        'So, when do I get *my* tickets?', 'Collecting your government benefits I see... :eyes:', 'Tickets for all!',
-        'Have some tickets! Perhaps see an action movie or win a game of Battlefield!', 'Yes, I\'m the Automatic Tau Machine...',
-        'Here are your tickets! Have a wonderful day!', 'Between you and me, I have an endless supply of this stuff.',
-        'Fun fact: I give double the amount of tickets on Sundays!', 'Here you go!']
+        msgs = ['Here are your credits!', 'Gosh, you\'re just using me for money, aren\'t you?', ':money_with_wings: :money_with_wings:',
+        'So, when do I get *my* credits?', 'Collecting your government benefits I see... :eyes:', 'credits for all!',
+        '<insert unfunny joke here, i ran out>', 'Yes, I\'m the Automatic Tau Machine...',
+        'Here are your credits! Have a wonderful day!', 'Between you and me, I have an endless supply of this stuff.',
+        'Fun fact: I give double the amount of credits on Sundays!', 'Here you go!']
 
         bal = self.bot.users_[ctx.author.id]['tickets']
         amt = 20
@@ -71,15 +70,15 @@ class Economy(commands.Cog):
             amt *= 2
         await self.bot.users_.update(ctx.author.id, 'tickets', bal+amt)
 
-        desc = f'You have collected {amt} tickets for a new balance of {emoji["tickets"]}**{bal+amt}**!'
+        desc = f'You have collected **{amt}** credits for a new balance of {emoji["tickets"]}**{bal+amt}**!'
         embed = Embed(description=desc)
 
         await ctx.send(ctx.author.mention + ' ' + random.choice(msgs), embed=embed)
 
-    @tickets.error
-    async def tickets_error(self, ctx, error):
+    @credits.error
+    async def credits_error(self, ctx, error):
         if isinstance(error, commands.CommandOnCooldown):
-            await ctx.send('You\'ve already collected your daily tickets!')
+            await ctx.send('You\'ve already collected your daily credits!')
 
 def setup(bot):
     bot.add_cog(Economy(bot))

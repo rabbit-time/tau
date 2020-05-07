a0240d28e73bdecdb447c0669a9225f40e30d8df
Henri Welter <30204467+Lumen-01@users.noreply.github.com>
2020-05-07 17:32:44 -0500

    version update

--- config-example.py
+++ config-example.py
@@ -4,4 +4,4 @@ passwd = 'your_passwd' # postgres password
 invite = 'https://discord.gg/invite' # Invite to server
 repo = 'https://github.com/Lumen-01/tau' # GitHub repo link
 token = 'DISCORD API TOKEN'
-version = '1.1.0'
+version = '2.0.0'

3470f123e1e47a23e13cfd837442619a2281efb7
Emily Hall <30204467+emisdumb@users.noreply.github.com>
2020-11-20 01:20:17 -0600

    moved config example to README

--- config-example.py
+++
@@ -1,7 +0,0 @@
-owner_id = 'your_user_id'
-admins = () # Bot admins (perm level 4)
-passwd = 'your_passwd' # postgres password
-invite = 'https://discord.gg/invite' # Invite to server
-repo = 'https://github.com/Lumen-01/tau' # GitHub repo link
-token = 'DISCORD API TOKEN'
-version = '2.0.0'
